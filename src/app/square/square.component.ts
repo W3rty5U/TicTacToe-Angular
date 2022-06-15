b43619da75e7825c3ed92674e54866829bc1957e
W3rty5U <filipm.gda@gmail.com>
2022-06-15 20:46:26 +0200

    formatted files using Prettier

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,13 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `
-    <button>{{ value }}</button>
-  `,
-  styles: ['button {width: 100%; height: 100%;}']
+  template: ` <button>{{ value }}</button> `,
+  styles: ['button {width: 100%; height: 100%;}'],
 })
 export class SquareComponent {
-
   @Input() value: string = '';
-
 }
